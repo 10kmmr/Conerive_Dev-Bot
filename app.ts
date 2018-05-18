4b8fd9176a8fa80ba3c2bc80af787e99d83c3dc9
prithvihv <hvprithvi09@gmail.com>
2018-05-19 00:50:43 +0530

    stable working *bots cant be fronds with hoomans *MONKEYBOTS feature missing

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 import * as admin from 'firebase-admin';
-import {userInfo} from "os";
+
 
 //Server configurations
 const app = express();
@@ -15,9 +15,11 @@ app.listen(process.env.PORT || 3000, () => {
 //Firebase configurations
 var serviceAccount = require('./Conerive-d52cd6e292fe.json');
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL:"https://roadtrip-app-efa3a.firebaseio.com/"
 });
 let db = admin.firestore();
+let Realdb = admin.database();
 
 app.get('/AddBot',async function (req, res) {
     let BotNumber = await BotCount();
@@ -25,6 +27,11 @@ app.get('/AddBot',async function (req, res) {
     await AddNewUsers(userPhonearray);
     await UpdateGeneralAllUsers(userPhonearray);
     await Updatecount(BotNumber +userPhonearray.length);
+    res.send({
+        "Status" :"Success",
+        "DONE":"DEAL",
+        "New BoT ": BotNumber
+    });
 });
 
 app.get('/AddBot_Trip',async function (req, res) {
@@ -36,6 +43,9 @@ app.get('/AddBot_Trip',async function (req, res) {
     for (let num of userPhonearray) {
         await AddTripto(num);
     }
+    res.send({
+        "writing":"data"
+    });
 });
 
 app.post("/AddFriendBot",async function(req,res){
@@ -47,19 +57,41 @@ app.post("/AddFriendBot",async function(req,res){
     }
     await AddFriendShipTo(MainBitch,SideBitch);
     await AddFriendShipTo(SideBitch,MainBitch);
+    res.send({
+        "Status" :"Success",
+        "DONE":"DEAL",
+        "NOW" : MainBitch + "AND " + SideBitch + " Are fronds ."
+    });
 });
 
 app.post("/AddTripBot",async function(req,res) {
-   let MainBitch = req.body.bot;
+   let MainBitch = req.body.trippimp;
    await AddTripto(MainBitch);
+   await AddLocation(MainBitch);
+   res.send({
+       "DONE":"DEAL",
+       "ADDED":MainBitch
+   })
 });
 
+
+async function AddLocation(bot){
+    return new Promise(resolve => {
+        Realdb.ref("/USERS/"+bot+"/Location").update({
+            "Latitude": 12.73 +Math.random(),
+            "Longitude": 77.6198776
+        }).then(()=>{
+            resolve();
+        });
+    });
+}
 async function AddTripto(bot){
     return new Promise(resolve => {
-        db.collection("TRIP").doc("Q3KmmOTZiyrFi9Yqf7jf").get().then(groupinfo=>{
+        db.collection("TRIPS").doc("cT3kfPzm2cw7uoDu27Ht").get().then(groupinfo=>{
+            console.log(groupinfo.data());
             let tripmemberArray : Array<string> = groupinfo.data().Users;
             tripmemberArray.push(bot);
-            db.collection("TRIP").doc("Q3KmmOTZiyrFi9Yqf7jf").update({ Users : tripmemberArray }).then(()=>resolve());
+            db.collection("TRIPS").doc("cT3kfPzm2cw7uoDu27Ht").update({ Users : tripmemberArray }).then(()=>resolve());
         });
     })
 }
@@ -67,6 +99,7 @@ async function AddFriendShipTo(id1,id2) {
     return new Promise(resolve => {
         db.collection("USERS").doc(id1).get().then(userInfo =>{
             let ArrayOfFriends:Array<string> = userInfo.data().Friends;
+            console.log(userInfo.data());
             ArrayOfFriends.push(id2);
            db.collection("USERS").doc(id1).update({ Friends :ArrayOfFriends}).then(()=>resolve());
         });
@@ -78,9 +111,11 @@ async function AddNewUsers(phones:Array<number>):Promise<boolean>{
             phones.forEach(PhonNum =>{
                 let userObject = {
                     "Name": "YenoBot"+PhonNum,
-                    "Phone": PhonNum
+                    "Phone": PhonNum.toString(),
+                    "Token":"NIG NIG",
+                    "Friends":[]
                 };
-                db.collection('USERS').doc(PhonNum).set(userObject).then(()=>{
+                db.collection('USERS').doc(PhonNum.toString()).set(userObject).then(()=>{
                     console.log("Bot Added" + PhonNum);
                 });
             })
@@ -91,7 +126,6 @@ async function AddNewUsers(phones:Array<number>):Promise<boolean>{
         resolve(true);
     })
 }
-
 async function BotCount():Promise<number>{
     return  new Promise<number>(resolve => {
         db.collection('BOT').doc('GENERAL').get().then(data =>{
@@ -101,17 +135,20 @@ async function BotCount():Promise<number>{
 }
 async function Updatecount(Count:number){
     return new Promise(resolve => {
-        db.collection('BOT').doc('GENERAL').update({ 'BOTCOUNT' : Count}).then(()=> console.log("New count is "+Count));
+        db.collection('BOT').doc('GENERAL').update({ 'BOTCOUNT' : Count}).then(()=>{
+            resolve();
+            console.log("New count is "+Count);
+        });
     });
 }
-
 async function UpdateGeneralAllUsers(phones:Array<number>){
     return new Promise(resolve =>{
-        db.collection("GENERAL").document("ALLUSERS").get().then(data =>{
+        db.collection("GENERAL").doc("ALLUSERS").get().then(data =>{
+            let ArrayPhones:Array<number> = data.data().Phone;
             phones.forEach(PhonNum=>{
-                data.data().Phone.push(PhonNum);
+                ArrayPhones.push(PhonNum);
             });
-            db.collection("GENERAL").document("ALLUSERS").update({Phone : phones}).then(()=>resolve());
+            db.collection("GENERAL").doc("ALLUSERS").update({Phone : ArrayPhones}).then(()=>resolve());
         })
     });
 }
